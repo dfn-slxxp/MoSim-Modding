1b75aa1f47bbd31a53ff85810e8224c8224ef47e
Bryce Roethel <me@bryceroethel.com>
2026-01-01 10:36:49 -0500

    Go git

---
+++ Assets/Prefabs/Reefscape/Robots/Mods/LynkModOfficial/9496/LynkSetpoint.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Prefabs.Reefscape.Robots.Mods.TestingMod._9496
+{
+    [CreateAssetMenu(fileName = "Setpoint", menuName = "Robot/Lynk Setpoint", order = 0)]
+    public class LynkSetpoint : ScriptableObject
+    {
+        [Tooltip("Inches")] public float elevatorHeight;
+    }
+}
